1c703ffaba17ac52e24a5f7e651c14cc08398a06
Walterli Valadares Jr. <walterli.valadares@outlook.com>
2025-05-25 15:49:47 -0700

    deploy

--- Payments.API/Controllers/v1/AsaasController.cs
+++ Payments.API/Controllers/v1/AsaasController.cs
@@ -1,7 +1,6 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Payments.API.Filters;
 using Payments.Domain.ApiModel;
 using Payments.Domain.Interfaces.Services;
 
