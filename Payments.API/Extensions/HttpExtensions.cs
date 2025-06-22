11082fc0562874c9194e2def67affe75925181b3
Walterli Valadares Jr. <walterli.valadares@outlook.com>
2025-06-21 18:56:26 -0700

    removendo configs desnecessarias

--- Payments.API/Extensions/HttpExtensions.cs
+++ Payments.API/Extensions/HttpExtensions.cs
@@ -10,16 +10,6 @@ public static class HttpExtensions
 {
     public static IServiceCollection AddCustomHttp(this IServiceCollection services, SecretManager secretManager)
     {
-        var url = secretManager.Asaas.Url;
-        var asaasToken = secretManager.Asaas.AcessToken;
-        services.AddHttpClient("api-asaas", client =>
-        {
-            client.BaseAddress = new Uri(url);
-            client.DefaultRequestHeaders.Add("User-Agent", "VanCoreAPI");
-            client.DefaultRequestHeaders.Add("access_token", asaasToken);
-            client.DefaultRequestHeaders.Add("Accept", "application/json");
-        });
-
         services.AddRefitClient<IAuthApi>()
             .ConfigureHttpClient(c => c.BaseAddress = new Uri(secretManager.URL.AuthAPI));
 
